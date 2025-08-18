b5c781cc11fd5cef8264fdea03e8d78fac911c29
hoangdanhnguyen.io <hoangdanh54317@gmail.com>
2025-08-18 06:11:20 +0000

    Edited Spark

--- src/vite-end.d.ts
+++ src/vite-end.d.ts
@@ -1,12 +1,3 @@
 /// <reference types="vite/client" />
 declare const GITHUB_RUNTIME_PERMANENT_NAME: string
 declare const BASE_KV_SERVICE_URL: string
-
-interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL?: string
-  readonly VITE_API_BASE?: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
